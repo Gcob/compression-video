7a2c3ca53f21c8301bf4f149f60fd78df56d9d9a
Jacob Proulx <jacobproulx@gmail.com>
2024-01-06 17:59:53 -0500

    seems to work

--- app.mjs
+++ app.mjs
@@ -1,15 +1,32 @@
 import ffmpeg from 'fluent-ffmpeg';
-import { glob } from 'glob';
 import path from 'path';
 import fs from 'fs';
-import config from './config.js'
+import config from './config.js';
 
 const inputFolder = config.inputFolder;
-const outputFolder = `${inputFolder}${config.outputFolderSuffix}`;
+const outputFolder = config.outputFolder;
 
 // Créer le dossier de sortie s'il n'existe pas
 if (!fs.existsSync(outputFolder)) {
   fs.mkdirSync(outputFolder);
+}
+
+// Fonction pour lister les fichiers récursivement
+function listFilesRecursively(dir, extensions, fileList = []) {
+  const files = fs.readdirSync(dir);
+
+  files.forEach(file => {
+    const filePath = path.join(dir, file);
+    if (fs.statSync(filePath).isDirectory()) {
+      listFilesRecursively(filePath, extensions, fileList);
+    } else {
+      if (extensions.some(ext => file.endsWith(`.${ext}`))) {
+        fileList.push(filePath);
+      }
+    }
+  });
+
+  return fileList;
 }
 
 // Fonction pour compresser une vidéo
@@ -18,9 +35,11 @@ function compressVideo(videoPath) {
     const outputFilePath = path.join(outputFolder, path.basename(videoPath, path.extname(videoPath)) + '.min.mp4');
 
     ffmpeg(videoPath)
-      .videoCodec('libx264')
-      .size('1280x720')
-      .outputOptions(['-crf 28', '-preset veryfast'])
+      .outputOptions([
+        '-vf', 'scale=-2:720', // redimensionne la vidéo à une hauteur de 720px tout en conservant le ratio d'aspect
+        '-crf 28', 
+        '-preset veryfast'
+      ])
       .on('end', () => {
         console.log(`Compression terminée : ${outputFilePath}`);
         resolve();
@@ -33,15 +52,14 @@ function compressVideo(videoPath) {
   });
 }
 
-// Trouver toutes les vidéos dans le dossier et ses sous-dossiers
-glob(`${inputFolder}/**/*.+(${config.videoFormats.join('|')})`, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
 
-  // Compresser les vidéos de manière asynchrone
-  Promise.all(files.map(file => compressVideo(file)))
-    .then(() => console.log('Toutes les vidéos ont été compressées.'))
-    .catch(error => console.error(`Erreur lors de la compression : ${error}`));
-});
+
+
+// Trouver toutes les vidéos dans le dossier et ses sous-dossiers
+const files = listFilesRecursively(inputFolder, config.videoFormats);
+console.log("Fichiers à compresser:", files);
+
+// Compresser les vidéos de manière asynchrone
+Promise.all(files.map(file => compressVideo(file)))
+  .then(() => console.log('Toutes les vidéos ont été compressées.'))
+  .catch(error => console.error(`Erreur lors de la compression : ${error}`));
